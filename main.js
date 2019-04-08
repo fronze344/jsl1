1d89fc17c3207c051b7562c0e6585af6c12eff8d
Jaeger <zerow.man@gmail.com>
2019-04-08 14:36:53 +0300

    Новый коммит

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var leftBorder = 1;
+let secongvar = 3;
+const pi = 3.13;
+
+console.log("stroka" + 'stroka2');
+
+
+var su4ka = {
+  name1: "serega",
+  who: "pidor",
+  age: 30
+};
+
+console.log(su4ka.name1, su4ka.who);
+
+var arr = [23, 22, 10];
+
+console.log(arr[0]);
+
+console.log("23" === "3222");
